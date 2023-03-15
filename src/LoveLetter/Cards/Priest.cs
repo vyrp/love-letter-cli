aa0fc129595074c8df232f818c39861cce94332f
Vyrp <3949802+vyrp@users.noreply.github.com>
2023-03-15 03:20:14 -0700

    Implement Priest.Play().

--- src/LoveLetter/Cards/Priest.cs
+++ src/LoveLetter/Cards/Priest.cs
@@ -1,9 +1,21 @@
-﻿namespace LoveLetter.Cards
+﻿using System;
+
+namespace LoveLetter.Cards
 {
     internal sealed class Priest : Card
     {
         public override string Name => nameof(Priest);
 
         public override byte Number => 2;
+
+        public override void Play(Game game, Player currentPlayer)
+        {
+            Console.WriteLine($"Playing {Name}...");
+            Player targetPlayer = game.ChooseNonActivePlayer();
+
+            Card hand = targetPlayer.Hand;
+            Console.WriteLine($"Player {targetPlayer.Name} has a {hand.Name} ({hand.Number}).");
+            Console.ReadLine();
+        }
     }
 }
